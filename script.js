79e0117769191ac432681ee6e4e7d0340db8c9c7
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-08-13 15:59:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	let change = { q: 0, d: 0, n: 0, p: 0 };
+
+  change.q = Math.floor(c / 25);
+  amount %= 25;
+
+  change.d = Math.floor(c / 10);
+  amount %= 10;
+
+  change.n = Math.floor(c / 5);
+  amount %= 5;
+
+  change.p = amount;
+
+  return change;
 };
 
 // Do not the change the code below
